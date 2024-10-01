adec9ee67530e5dcf6ba5041ddda6facafacad22
Nikita George <nikkigeorge2@gmail.com>
2024-10-01 11:24:05 -0600

    Added color types for 3D iPhone and size types.

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//import { useGSAP } from "@gsap/react"
+//import gsap from "gsap";
+
+const ModelView = () => {
+    return (
+        <div>ModelView</div>
+    )
+}
+
+export default ModelView
